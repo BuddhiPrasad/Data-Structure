53f5e9d24dc77e01cd3a18e6192c106b5aebb0f2
Buddhi Prasad <Buddhi Prasad@PrasadZ>
2021-01-17 09:56:38 +0530

    0000

---
+++ myproject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Buddhi Prasad
+ *
+ */
+module myproject {
+}
---
+++ myproject/src/myproject/MyPor.java
@@ -0,0 +1,12 @@
+package myproject;
+
+public class MyPor {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world");
+		System.out.println("HI");
+
+	}
+
+}
